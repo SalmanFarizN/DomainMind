2ffbbe9c0ed33f8da235df8a2a900b7a986f7d65
SalmanFarizN <salmanfarizn@gmail.com>
2025-08-24 17:14:26 +0200

    Refactor RAG QA functions to separate non-streaming and streaming versions; enhance streaming function to process <think> tags and yield responses.

--- src/Interface.py
+++ src/Interface.py
@@ -7,7 +7,9 @@ from src.OutputProcess import split_think_and_answer, get_thought
 from src.OutputProcess import remove_details_blocks
 
 
-def rag_qa_multi_query(message, history):
+def rag_qa_multi_query_final(message, history):
+    """Non-streaming version of the RAG QA function"""
+
     # Convert Gradio history format to LangChain message format
     history_langchain_format = []
     # Convert Gradio history (list of {"role": ..., "content": ...}) to LangChain format
@@ -34,9 +36,49 @@ def rag_qa_multi_query(message, history):
         return {"role": "assistant", "content": f"❌ Error: {str(e)}"}
 
 
+def rag_qa_multi_query_streaming(message, history):
+    """Streaming version of the RAG QA function"""
+    # Convert Gradio history format to LangChain message format
+    history_langchain_format = []
+    # Convert Gradio history (list of {"role": ..., "content": ...}) to LangChain format
+    for turn in history:
+        if turn["role"] == "user":
+            history_langchain_format.append(HumanMessage(content=turn["content"]))
+        elif turn["role"] == "assistant":
+            cleaned_content = remove_details_blocks(turn["content"])
+            history_langchain_format.append(AIMessage(content=cleaned_content))
+    try:
+        raw_output = ""
+        processed_full_output = ""
+
+        # Stream the response per token and process <think> tags
+        # for thinking models
+        for chunk in rag_chain_multi_query.stream(
+            {"history": history, "question": message}
+        ):
+            raw_output += chunk
+
+            # Check for <think> start
+            if "<think>" in chunk:
+                chunk = chunk.replace("<think>", "Reasoning:")
+
+            if "</think>" in chunk:
+                chunk = chunk.replace("</think>", "End of Reasoning\n")
+
+            processed_full_output += chunk
+
+            yield {
+                "role": "assistant",
+                "content": processed_full_output,
+            }
+
+    except Exception as e:
+        return {"role": "assistant", "content": f"❌ Error: {str(e)}"}
+
+
 def launch_interface():
     demo = gr.ChatInterface(
-        fn=rag_qa_multi_query,
+        fn=rag_qa_multi_query_streaming,
         type="messages",
         title="DomainMind",
         description="Powered by RAG + Qwen3:8B",
