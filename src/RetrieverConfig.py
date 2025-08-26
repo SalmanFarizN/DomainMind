0aa411cf9f75b69f2e6c2ba7fba61ce533dfa358
SalmanFarizN <salmanfarizn@gmail.com>
2025-08-26 13:54:53 +0200

    different VectorDB load

--- src/RetrieverConfig.py
+++ src/RetrieverConfig.py
@@ -2,5 +2,5 @@ from src.VectorDB import VectorDB
 
 # Load the VectorDB and create a retriever
 vectordb = VectorDB()
-vectordb.load_vectorstore(persist_directory="data/sampledoc_vectordb")
+vectordb.load_vectorstore(persist_directory="data/doc_vectordb")
 retriever = vectordb.create_retriever()
