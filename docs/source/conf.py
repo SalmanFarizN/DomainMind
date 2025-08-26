18169faba67da276d770df8e0384aa5b104d7b5c
SalmanFarizN <salmanfarizn@gmail.com>
2025-08-26 16:53:19 +0200

    Reorder sys.path insertions in conf.py for clarity and consistency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,9 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath("../../src"))
-# sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src"))
+
 
 project = "DomainMind"
 copyright = "2025, Salman Fariz Navas"
